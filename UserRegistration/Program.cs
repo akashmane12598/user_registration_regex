a707889840e6cf4a2e890cb78235a1485e90a0c1
Akash <akashmane12598@gmail.com>
2020-10-05 15:59:23 +0530

    [Akash] Add. First Name Validation UC1

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -7,6 +8,17 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            Console.WriteLine("Enter your first name(only first letter in CAPS): ");
+            string fname = Console.ReadLine();
+            string name_regex = @"^[A-Z]{1}[a-z]{2,}";
+            if (Regex.IsMatch(fname, name_regex))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+            }
         }
     }
 }
