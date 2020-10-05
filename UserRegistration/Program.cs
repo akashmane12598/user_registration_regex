2ef3715300cb3cb1acdffc7db433818443c4539d
Akash <akashmane12598@gmail.com>
2020-10-05 16:28:06 +0530

    [Akash] Add. First Name Validation UC1

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,17 +8,39 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
-            Console.WriteLine("Enter your first name(only first letter in CAPS): ");
-            string fname = Console.ReadLine();
-            string name_regex = @"^[A-Z]{1}[a-z]{2,}";
-            if (Regex.IsMatch(fname, name_regex))
-            {
-                Console.WriteLine("First Name is valid");
+            string fname, lname;
+            string name_regex;
+            while (true)
+            {         
+                Console.WriteLine("Enter your first name(only first letter in CAPS): ");
+                fname = Console.ReadLine();
+                name_regex = @"^[A-Z]{1}[a-z]{2,}";
+                if (Regex.IsMatch(fname, name_regex))
+                {
+                    Console.WriteLine("Valid First Name");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid First Name");
+                }
             }
-            else
+            while (true)
             {
-                Console.WriteLine("First Name is Invalid");
+                Console.WriteLine("Enter your last name(only first letter in CAPS): ");
+                lname = Console.ReadLine();
+                name_regex = @"^[A-Z]{1}[a-z]{2,}";
+                if (Regex.IsMatch(lname, name_regex))
+                {
+                    Console.WriteLine("Valid Last Name");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Last Name");
+                }
             }
+            Console.WriteLine("Your Valid Entered Name is "+fname+" "+lname);
         }
     }
 }
