4d78c7cd39ebb3e594c5cf05d02172883b5977a0
Akash <akashmane12598@gmail.com>
2020-10-05 19:25:16 +0530

    [Akash] Add. Password rule3 Validation UC7

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -78,7 +78,7 @@ namespace UserRegistration
             {
                 Console.WriteLine("Set a password: ");
                 password = Console.ReadLine();
-                password_regex = @"((?=.*[A-Z])(?=.*\w).{8,})";
+                password_regex = @"((?=.*[A-Z])(?=.*\w)(?=.*\d).{8,})";
                 if (Regex.IsMatch(password, password_regex))
                 {
                     Console.WriteLine("Password set Successfully");
