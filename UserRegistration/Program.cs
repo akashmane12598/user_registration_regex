521c04ecd7dbdeb04b33a647a1f424bdc5fe82a1
Akash <akashmane12598@gmail.com>
2020-10-11 16:21:01 +0530

    [Akash] Add. MS testing UC10

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -3,95 +3,121 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static string fname, lname, email, phone_no, password;
+        public static string name_regex;
+        public static string regex_email;
+        public static string phone_no_regex;
+        public static string password_regex;
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
-            string fname, lname,email,phone_no,password;
-            string name_regex;
-            string regex_email;
-            string phone_no_regex;
-            string password_regex;
+            //FirstNameRegex();
+            
+            Console.WriteLine("Your Valid Entered Name is " + fname + " " + lname);
+            Console.WriteLine("Your Valid Entered Email-Id is " + email);
+            Console.WriteLine("Your Valid Entered Phone Number is " + phone_no);
+        }
+        public Boolean FirstNameRegex()
+        {
             while (true)
-            {         
+            {
                 Console.WriteLine("Enter your first name(only first letter in CAPS): ");
-                fname = Console.ReadLine();
+                fname = "Akash";
                 name_regex = @"^[A-Z]{1}[a-z]{2,}$";
                 if (Regex.IsMatch(fname, name_regex))
                 {
                     Console.WriteLine("Valid First Name");
-                    break;
+                    return true;
+                    
                 }
                 else
                 {
                     Console.WriteLine("Invalid First Name");
+                    return false;
                 }
             }
+        }
+
+        public Boolean LastNameRegex()
+        {
             while (true)
             {
                 Console.WriteLine("Enter your last name(only first letter in CAPS): ");
-                lname = Console.ReadLine();
+                lname = "Mane";
                 name_regex = @"^[A-Z]{1}[a-z]{2,}$";
                 if (Regex.IsMatch(lname, name_regex))
                 {
                     Console.WriteLine("Valid Last Name");
-                    break;
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Invalid Last Name");
+                    return false;
                 }
             }
-            
+        }
+
+        public Boolean EmailId()
+        {
             while (true)
             {
                 Console.WriteLine("Enter your Email-id: ");
-                email = Console.ReadLine();
+                email = "akash.mane@spit.ac.in";
                 regex_email = @"^[A-Za-z]+[.+-]?[a-z]*[@][A-Za-z0-9]+[.][a-z]{2,}([.][a-z]{2})?$";
                 if (Regex.IsMatch(email, regex_email))
                 {
-                    Console.WriteLine("Valid Email-Id: "+email);
-                    break;
+                    Console.WriteLine("Valid Email-Id: " + email);
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Invalid Email-Id");
+                    return false;
                 }
             }
+        }
+        
+        public Boolean PhoneNumber()
+        {
             while (true)
             {
                 Console.WriteLine("Enter the Valid Phone Number: ");
-                phone_no = Console.ReadLine();
+                phone_no = "+91 0123456789";
                 phone_no_regex = @"^[+0-9]{2,}[\s]{1}[0-9]{10}$";
                 if (Regex.IsMatch(phone_no, phone_no_regex))
                 {
-                    Console.WriteLine("Valid Phone Number: "+phone_no);
-                    break;
+                    Console.WriteLine("Valid Phone Number: " + phone_no);
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Invalid Phone Number");
+                    return false;
                 }
             }
+        }
+
+        public Boolean Password()
+        {
             while (true)
             {
                 Console.WriteLine("Set a password: ");
-                password = Console.ReadLine();
+                password = "Axdcs55@gfS";
                 password_regex = @"(?=.*[A-Z])(?=.*\w)(?=.*\d)(?=[^@$!%*^#?&]*[@$!%*^#?&][^@$!%^*#?&]*$).{8,}$";
                 if (Regex.IsMatch(password, password_regex))
                 {
                     Console.WriteLine("Password set Successfully");
-                    break;
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Enter password again");
+                    return false;
                 }
             }
-            Console.WriteLine("Your Valid Entered Name is " + fname + " " + lname);
-            Console.WriteLine("Your Valid Entered Email-Id is " + email);
-            Console.WriteLine("Your Valid Entered Phone Number is " + phone_no);
         }
     }
 }
